081bc863e2e8d1617d84d415bbb46221bff80808
ameliagan <ameliagan@gsd.harvard.edu>
2023-03-13 11:23:41 -0400

    Cube to Particles

--- Cube Moving and Decay/src/script.js
+++ Cube Moving and Decay/src/script.js
@@ -1,93 +1,73 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { DragControls } from 'three-dragcontrols'
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// import GUI from 'lil-gui'; 
 
-// Initialize variables
-var container, scene, camera, renderer, dragControls, cube, particles, trace;
-var width = window.innerWidth;
-var height = window.innerHeight;
+let scene;
+let camera;
+let renderer;
 
-var orbit;
+//grid
+scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(100000, 100, 0x424242, 0x424242));
 
-// Set up the scene
-init();
+// create the cube geometry and material
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    roughness: 0.5,
+    metalness: 0.5,
+});
 
-function init() {
+// create the cube mesh and add it to the scene
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-    // Create container and add it to the DOM
-    container = document.createElement( 'div' );
-    document.body.appendChild( container );
+// set the decay rate
+const decayRate = 0.01;
 
-    // Set up the camera
-    camera = new THREE.PerspectiveCamera( 75, width / height, 1, 1000 );
-    camera.position.set( 0, 0, 5 );
+// define a noise texture for the cube
+const noiseTexture = new THREE.TextureLoader().load('noise.jpg');
+noiseTexture.wrapS = THREE.RepeatWrapping;
+noiseTexture.wrapT = THREE.RepeatWrapping;
 
-    // Set up the scene
-    scene = new THREE.Scene();
+// define a displacement map for the cube
+const displacementMap = new THREE.TextureLoader().load('displacement.jpg');
+displacementMap.wrapS = THREE.RepeatWrapping;
+displacementMap.wrapT = THREE.RepeatWrapping;
 
-    // Set up the renderer
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( width, height );
-    container.appendChild( renderer.domElement );
+// create a shader material for the cube
+const shaderMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+        time: { value: 0 },
+        noiseTexture: { value: noiseTexture },
+        displacementMap: { value: displacementMap },
+    },
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+});
 
-    // Add a cube to the scene
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-    cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+// set the material of the cube to the shader material
+cube.material = shaderMaterial;
 
-    // Add particles to the scene
-    particles = new THREE.Group();
-    for ( var i = 0; i < 100; i++ ) {
-        var particleGeometry = new THREE.SphereGeometry( 0.05, 16, 16 );
-        var particleMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-        var particle = new THREE.Mesh( particleGeometry, particleMaterial );
-        particles.add( particle );
-    }
-    scene.add( particles );
+// define the update function
+function update() {
+    // update the time uniform of the shader material
+    shaderMaterial.uniforms.time.value += 0.01;
 
-    // Add a trace to the scene
-    trace = new THREE.Group();
-    scene.add( trace );
+    // reduce the size of the cube
+    cube.scale.multiplyScalar(1 - decayRate);
 
-    // Set up drag controls for the cube
-    dragControls = new DragControls( [ cube ], camera, renderer.domElement );
+    // update the camera position to follow the cube
+    camera.position.set(cube.position.x + 5, cube.position.y + 5, cube.position.z + 5);
+    camera.lookAt(cube.position);
 
-    // Start the animation loop
-    animate();
-
+    // render the scene
+    renderer.render(scene, camera);
 }
 
-orbit = new OrbitControls(camera, renderer.domElement);
-orbit.update();
-orbit.addEventListener('change', render);
-
-
+// call the update function in the animation loop
 function animate() {
-
-    requestAnimationFrame( animate );
-
-    // Decay the cube into particles
-    if ( cube.scale.x > 0.1 ) {
-        cube.scale.x -= 0.01;
-        cube.scale.y -= 0.01;
-        cube.scale.z -= 0.01;
-        for ( var i = 0; i < particles.children.length; i++ ) {
-            particles.children[ i ].position.copy( cube.position );
-            particles.children[ i ].position.x += ( Math.random() - 0.5 ) * 0.5;
-            particles.children[ i ].position.y += ( Math.random() - 0.5 ) * 0.5;
-            particles.children[ i ].position.z += ( Math.random() - 0.5 ) * 0.5;
-        }
-    }
-
-    // Add a new trace point to the scene
-    var traceGeometry = new THREE.SphereGeometry( 0.01, 16, 16 );
-    var traceMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-    var tracePoint = new THREE.Mesh( traceGeometry, traceMaterial );
-    tracePoint.position.copy( cube.position );
-    trace.add( tracePoint );
-
-    // Render the scene
-    renderer.render( scene, camera );
-
+    requestAnimationFrame(animate);
+    update();
 }
+animate();
